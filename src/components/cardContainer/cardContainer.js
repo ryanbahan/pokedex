078486a567abd163c72cf148220e6cc626513d8e
Ryan Bahan <ryan@ryanbahan.com>
2020-06-21 14:10:11 -0600

    Add grid for pokemon and dynamically generate info from props on pokemonCard

--- src/components/cardContainer/cardContainer.js
+++ src/components/cardContainer/cardContainer.js
@@ -1,6 +1,7 @@
 import PokemonCard from '../pokemonCard/pokemonCard'
 import { useContext } from 'react'
 import { PokemonContext } from '../../contexts/pokemonContext'
+import styled from 'styled-components'
 
 const CardContainer = () => {
     const PokemonStore = useContext(PokemonContext)
@@ -8,12 +9,18 @@ const CardContainer = () => {
     const { pokemon } = PokemonStore.state
 
     return (
-        <main>
+        <Main>
             {pokemon.map(p => (
                 <PokemonCard key={p.id} {...p} />
             ))}
-        </main>
+        </Main>
     )
 }
+
+const Main = styled.main`
+    border: solid 1px blue;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+`
 
 export default CardContainer
--- src/components/pokemonCard/pokemonCard.js
+++ src/components/pokemonCard/pokemonCard.js
@@ -1,8 +1,24 @@
-const PokemonCard = (props) => {
-    console.log(props)
+const PokemonCard = ({
+    height,
+    id,
+    img,
+    multipliers,
+    name,
+    num,
+    type,
+    weaknesses,
+    weight,
+}) => {
     return (
-        <section>
-            pokemonCard
+        <section style={{border: "solid 1px black"}}>
+            <p>{ name }</p>
+            <img src={img} />
+            <p>number: { num }</p>
+            <p>type: { type }</p>
+            <p>weaknesses: { weaknesses }</p>
+            <p>height: { height }</p>
+            <p>weight: { weight }</p>
+            <p>multipliers: { multipliers }</p>
         </section>
     )
 }
